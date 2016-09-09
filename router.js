3838f361582c84cf1703a30b5833c41beb7bc0cd
SKM-MAC <social@saadkhanmalik.com>
2016-09-09 23:28:52 +0300

    HTTP Methods and Headers (Completed)

--- router.js
+++ router.js
@@ -1,19 +1,35 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
-
+var querystring = require("querystring");
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
   if(request.url === "/") {
-    //show search
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    renderer.view("header", {}, response);
-    renderer.view("search", {}, response);
-    renderer.view("footer", {}, response);
-    response.end();
+    if(request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      renderer.view("header", {}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    } else {
+      //if url == "/" && POST
+
+      //get the post data from body
+      request.on("data", function(postBody){
+        //console.log(postBody.toString());
+        //extract the username
+        var query = querystring.parse(postBody.toString()); // this will return a jason object back.
+        //redirect to /:username
+        response.writeHead(303, {"Location": "/" + query.username});
+        response.end();
+
+      });
+
+
+
+    }
   }
-  //if url == "/" && POST
-    //redirect to /:username
 }
 
 //Handle HTTP route GET /:username i.e. /chalkers
@@ -21,7 +37,7 @@ function user(request, response) {
   //if url == "/...."
   var username = request.url.replace("/", "");
   if(username.length > 0) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     renderer.view("header", {}, response);
 
     //get json from Treehouse
